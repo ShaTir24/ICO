412c7129b74648533fa7904ac20847b18a0f4a51
Tirth Shah <tirthshah100@gmail.com>
2023-03-12 13:42:00 +0530

    initial commit with interface

---
+++ hardhat_tutorial/contracts/ICryptoDevs.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Indicator: MIT
+pragma solidity ^0.8.17;
+
+interface ICryptoDevs {
+    //functions that we will be calling from CryptoDevs contract
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns(uint256 tokenId);
+    function balanceOf(address owner) external view returns(uint256 balance);
+}
